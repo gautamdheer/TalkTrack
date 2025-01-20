8367a68a13002b297b1696314afe258a93ec4b2d
Gautam Dheer <gautam.litesh@gmail.com>
2025-01-20 23:24:38 +0530

    updated dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,5 +13,13 @@
                 </div>
             </div>
         </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("Hello!") }} {{auth()->user()->name }}
+        
+                </div>
+            </div>
     </div>
 </x-app-layout>
